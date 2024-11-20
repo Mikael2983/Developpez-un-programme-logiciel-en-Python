ff37f81c85fef5d697b7ac1db92bddd27dd23e93
Mikael2983 <mikael.picolo@laposte.net>
2024-11-20 14:37:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import utils
+
+
+if __name__ == "__main__":
+
+    url_site = "https://books.toscrape.com/"
+
+    url_categories = utils.scrape_all_categories(url_site)
+
+    for category in url_categories:
+        url_books = utils.scrape_category(category,url_categories[category])
+        utils.write_data_on_csv(url_books,category)
