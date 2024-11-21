2dc3016c59912e0e30716f585bbef42c2663e5f5
Mikael Picolo <mikael.picolo@laposte.net>
2024-11-21 16:31:16 +0100

    passage en POO

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import utils
-
+from pathlib import Path
 
 if __name__ == "__main__":
 
     url_site = "https://books.toscrape.com/"
 
-    url_categories = utils.scrape_all_categories(url_site)
+    site = utils.Librairy(url_site)
+    
+    for category_name in site.url_categories:
+        category = utils.Category(category_name,site.url_categories[category_name])
+        
+        repertory = Path("data/" + category.name + "/images").resolve()
+        repertory.mkdir(parents=True, exist_ok=True)
+        books = []
+        
+        for url in category.list:
+            book= utils.Book(category.name,url)
+            utils.save_cover_picture(book)
+            books.append(book)
 
-    for category in url_categories:
-        url_books = utils.scrape_category(category,url_categories[category])
-        utils.write_data_on_csv(url_books,category)
+        utils.write_data_on_csv(books,category.name)
